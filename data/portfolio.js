29fb2c8af0a1f956572d22c4150eca91fb8fb005
David Peralez <davidperalez89@gmail.com>
2024-05-10 19:46:33 -0700

    created portfolio items and array of objects to display. Adding styling

---
+++ data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Quote App",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "JavaScript", "Tailwind"],
+    link: "https://https://github.com/DPeralez1/quote-app",
+  },
+  {
+    title: "F1 project",
+    imgUrl: "",
+    stack: ["React", "Bootstrap"],
+    link: "https://github.com/DPeralez1/f1-project",
+  },
+  {
+    title: "Portfolio",
+    imgUrl: "",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "https://github.com/DPeralez1/portfolio",
+  },
+];
---
+++ src/components/PortfolioItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function PortfolioItem({ title, imgUrl, stack, link }) {
+  return (
+    <div className="border-2 border-black rounded-md overflow-hidden">
+      <img
+        src={imgUrl}
+        alt="portfolio img"
+        className="w-full h-36 md:h-8 object-cover cursor-pointer"
+      />
+      <div className="w-full p-4">
+        <h3 className="text-lg md:trext-xl mb-2 md:mb-3 font-semibold">
+          {title}
+        </h3>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+          {stack.map((item) => (
+            <span className="inline-block px-2 py-1 font-semibold border-4 border-double border-violet-600 rounded-md">
+              {item}
+            </span>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
+}
