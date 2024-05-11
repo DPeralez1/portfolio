1897e13a64fd7f33f327b16e827448d4ef2d81eb
David Peralez <davidperalez89@gmail.com>
2024-05-10 19:59:14 -0700

    added images for projects and left note to moving folder in portfolio.js

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,20 +1,22 @@
 export default [
   {
     title: "Quote App",
-    imgUrl: "",
+    imgUrl: "/assets/Quote.png",
     stack: ["HTML", "CSS", "JavaScript", "Tailwind"],
     link: "https://https://github.com/DPeralez1/quote-app",
   },
   {
     title: "F1 project",
-    imgUrl: "",
+    imgUrl: "/assets/F1.png",
     stack: ["React", "Bootstrap"],
     link: "https://github.com/DPeralez1/f1-project",
   },
   {
     title: "Portfolio",
-    imgUrl: "",
+    imgUrl: "/assets/Portfolio.png",
     stack: ["React", "Tailwind", "Vite"],
     link: "https://github.com/DPeralez1/portfolio",
   },
 ];
+
+// assets folder is in the public folder because images dont show once I run build.
