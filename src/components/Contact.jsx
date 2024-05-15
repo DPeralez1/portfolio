2bf656f87e4e1a62712881af4fc97f083ad469d6
David Peralez <davidperalez89@gmail.com>
2024-05-14 19:17:58 -0700

    created contact form with name, email and message. Used getform to submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 
-export default function C0ntact() {
-  return <div>Contact</div>;
+export default function Contact() {
+  return (
+    <div className="flex flex-col mb-10 mx-auto">
+      <div className="flex justify-center items-center">
+        <form
+          action="https://getform.io/f/warkwwpb"
+          method="POST"
+          className="flex flex-col w-full md:w-7/12"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows="10"
+            className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <button
+            type="button"
+            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-violet-600 hover:bg-violet-800"
+          >
+            Work With Me
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
