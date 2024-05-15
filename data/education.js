0fef84d2e5bb9311a7ca96bbfc78a5aaf40e847d
David Peralez <davidperalez89@gmail.com>
2024-05-14 18:36:44 -0700

    created education section and added information.

--- data/education.js
+++ data/education.js
@@ -1,8 +1,13 @@
 export default [
   {
-    year: "Present",
+    year: "Jan. 15th - Present",
     name: "Nucamp",
     degree: "FullStack Web and Mobile Developer",
+  },
+  {
+    year: "Nov. 27th - Dec. 23rd 2023",
+    name: "Nucamp",
+    degree: "Web Development Fundamentals",
   },
   {
     year: "2014-2016",
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import EducationItem from "./EducationItem";
+import education from "../../data/education";
 
 export default function Education() {
-  return <div>Education</div>;
+  return (
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        {education.map((item) => (
+          <EducationItem
+            year={item.year}
+            name={item.name}
+            degree={item.degree}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
