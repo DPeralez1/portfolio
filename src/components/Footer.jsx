7d88e5434742c9ae5e650867edc273cec5188cc4
David Peralez <davidperalez89@gmail.com>
2024-05-14 19:21:29 -0700

    Added footer information. very minimal with date and year.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="py-5 text-center">
+      <p className="text-sm mt-2 opacity-25">
+        &copy; {new Date().getFullYear()} David Peralez. BioDigital Jazz Man!
+      </p>
+    </div>
+  );
 }
